94b9ef2c2afc6789758ebd9c4106ca991d8daf2e
Philippe Gerber <philippe.gerber4@gmail.com>
2026-06-04 09:24:30 +0200

    cleared boiler code and displayed title

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('tafy-movie-rating');
+  protected readonly title = signal('Movie Rating!');
 }
